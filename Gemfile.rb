aca803362bddedd72ad951f940be1a56077042ab
Gregory Hoggard <greg.hoggard@stelligent.com>
2020-04-16 18:10:18 -0400

    Gemfile

---
+++ Gemfile.rb
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'bundler'
+
+group :lint do
+  gem 'rubocop'
+  gem 'yaml-lint'
+end
+
+group :testing do
+  gem 'concurrent-ruby'
+  gem 'inspec'
+  gem 'kitchen-ansiblepush'
+  gem 'kitchen-ec2'
+  gem 'kitchen-inspec', github: 'inspec/kitchen-inspec', ref: '09976b4'
+  gem 'test-kitchen'
+
+  gem 'winrm'
+  gem 'winrm-elevated'
+  gem 'winrm-fs'
+end
